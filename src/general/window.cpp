97b4f015c794a923e34922513df0191e4a8762c3
Sylvain DURAND <sylvain.durand7@gmail.com>
2025-11-15 15:46:07 +0100

    Remove forced X11

--- src/general/window.cpp
+++ src/general/window.cpp
@@ -10,8 +10,6 @@ Window::Window()
     : _width { 1600 }
     , _height { 800 }
 {
-    // TODO: delete
-    glfwInitHint(GLFW_PLATFORM, GLFW_PLATFORM_X11);
     if (!glfwInit()) {
         NH3D_ABORT("GLFW init failed");
     }
